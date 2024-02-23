249df80d4b4c526a321a773ff338a971bb0f7104
MPlugins <github@mplugins.net>
2024-02-23 03:36:35 +0100

    Add uuid field to PlayerLocationInformation

--- src/main/java/net/mplugins/geoutil/PlayerLocationInformation.java
+++ src/main/java/net/mplugins/geoutil/PlayerLocationInformation.java
@@ -2,7 +2,11 @@ package net.mplugins.geoutil;
 
 import net.mplugins.geoutil.model.Country;
 
+import java.util.UUID;
+
 public interface PlayerLocationInformation {
+    UUID getPlayer();
+
     String getCity();
 
     Country getCountry();
