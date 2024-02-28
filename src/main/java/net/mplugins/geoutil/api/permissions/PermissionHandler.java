adc5525622ff080ada4e50b39b1111b25d29a4bd
MPlugins <github@mplugins.net>
2024-02-28 13:24:48 +0100

    PermissionHandler#allowJoin

--- src/main/java/net/mplugins/geoutil/api/permissions/PermissionHandler.java
+++ src/main/java/net/mplugins/geoutil/api/permissions/PermissionHandler.java
@@ -1,6 +1,14 @@
 package net.mplugins.geoutil.api.permissions;
 
 public interface PermissionHandler {
+
+    /**
+     * Allow users from a specific country to join this server.
+     *
+     * @param countryCode the two letter country-code for the region to allow
+     */
+    void allowJoin(String countryCode);
+
     /**
      * Prevent users from a specific country from joining this server (geo-blocking).
      *
